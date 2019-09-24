ebc1aec683ff9d423a7b1c6d9d0d49239bec3626
Takagi Kensuke <janus.wel.3@gmail.com>
2019-09-24 14:36:41 +0900

    :wrench: generate source map in dev mode

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,6 +7,7 @@ const config = merge(baseConfig, {
   entry: './src/index.dev.ts',
   mode: 'development',
   watch: true,
+  devtoo: 'inline-source-map',
   resolve: {
     extensions: ['.ts', '.js']
   },
