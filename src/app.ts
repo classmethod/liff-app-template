a921e91b676ce005054b74d7307e58b5f238e86c
Takagi Kensuke <janus.wel.3@gmail.com>
2019-09-25 18:12:14 +0900

    :sparkles: add procedures to liff.init

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+window.liff.init(
+  data => {
+    const {userId} = data.context
+    const accessToken = window.liff.getAccessToken()
+    console.log(userId, accessToken)
+  },
+  error => {
+    console.log(error.message)
+  },
+)
+
 export default () => {
   console.log('aaa')
   return 'aaa'
